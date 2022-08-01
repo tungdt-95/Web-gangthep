0620cd04bd9718bdea90dd8fe9caac614f19978d
teo <teo@gmail.com>
2022-08-01 17:17:58 +0700

    updatecode

---
+++ index.js
@@ -0,0 +1,16 @@
+       $('.parallax-window').parallax({imageSrc: 'paralax.jpg'});
+ // -------------scrolltotop-------------//
+ $(document).ready(function(){
+       $(window).scroll(function(){
+           if ($(this).scrollTop() > 100) {
+               $('.scrollup').fadeIn();
+           } else {
+               $('.scrollup').fadeOut();
+           }
+       }); 
+
+       $('.scrollup').click(function(){
+           $("html, body").animate({ scrollTop: 0 }, 600);
+           return false;
+       });	
+  });
